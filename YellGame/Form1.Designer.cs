11043d4d5a5c242942da5cb422a1942c21ab88f7
Alecsandro <alessandro.bevilacqua@issgreppi.it>
2022-05-30 13:26:12 +0200

    Add project files.

---
+++ YellGame/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace YellGame {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.Image = ((System.Drawing.Image)(resources.GetObject("ball.Image")));
+            this.ball.Location = new System.Drawing.Point(379, 177);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(50, 50);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(379, 69);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(50, 20);
+            this.label.TabIndex = 1;
+            this.label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.ball);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ YellGame/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YellGame {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        bool up = false;
+        bool down = false;
+        bool left = false;
+        bool right = false;
+
+        private void timer_Tick(object sender, EventArgs e) {
+            if (right && ball.Left < this.Width - ball.Width) {
+                ball.Left += Math.Min(10, this.Width - ball.Width - ball.Left);
+            } else if (left && ball.Left > 0) {
+                ball.Left -= Math.Min(10, ball.Left);
+            } 
+            
+            if (up && ball.Top > 0) {
+                ball.Top -= Math.Min(10, ball.Top);
+            } else if (down && ball.Top + ball.Height < this.Height) {
+                ball.Top += Math.Min(10, this.Height - ball.Height - ball.Top);
+            }
+
+            label.Text = ball.Left + ", " + ball.Top + "\n" +
+                this.Width + ", " + this.Height;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            switch (e.KeyCode) {
+                case Keys.Up:
+                    up = true;
+                    break;
+                case Keys.Down:
+                    down = true;
+                    break;
+                case Keys.Right:
+                    right = true;
+                    break;
+                case Keys.Left:
+                    left = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e) {
+            switch (e.KeyCode) {
+                case Keys.Up:
+                    up = false;
+                    break;
+                case Keys.Down:
+                    down = false;
+                    break;
+                case Keys.Right:
+                    right = false;
+                    break;
+                case Keys.Left:
+                    left = false;
+                    break;
+            }
+        }
+    }
+}
